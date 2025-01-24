aab97b2cdaa275653f23bc98d3e5a8100fa6c1d8
nuyar <qnfkdbs0222@naver.com>
2025-01-24 13:18:27 +0000

    feat: 04.부트 완료

---
+++ src/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -xue
+
+QEMU=qemu-system-riscv32
+
+# clang 경로와 컴파일 옵션
+CC=clang  # Ubuntu 등 환경에 따라 경로 조정: CC=clang
+CFLAGS="          \
+ -std=c11         \
+ -O2  -g3         \
+ -Wall -Wextra    \
+ --target=riscv32 \
+ -ffreestanding -nostdlib \
+"
+
+# 커널 빌드
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+$QEMU               \
+ -machine virt      \
+ -bios default      \
+ -nographic         \
+ -serial mon:stdio  \
+ --no-reboot        \
+ -kernel kernel.elf
